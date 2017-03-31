6e5ed6cfeedf93fb73cbdbb3c85fd755a3bf351d
Youcef Laidi <yocef2@gmail.com>
2017-03-31 13:12:10 -0400

    Added more parameters to Search

--- src/Search.java
+++ src/Search.java
@@ -32,26 +32,46 @@ public class Search {
 	private static HttpTransport httpTransport;
 	private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 
-	private String departure; // Airport or city IATA designator of the origin.	
-	private String destination; // Airport or city IATA designator of the destination.	
+	private String departure; // Airport or city IATA designator of the origin.
+	private String destination; // Airport or city IATA designator of the
+								// destination.
 	private String departureDate; // Departure date in YYYY-MM-DD format.
-	private int maxPrice;
+	private String maxPrice;
 	private int adultCount;
 	private int childCount;
+	private int infantInLapCount;
+	private int infantInSeatCount;
 	private int seniorCount;
 	private int maxStops;
-	private int maxConnectionDuration; // The longest connection between two legs, in minutes
-	//private int solutions; // he number of solutions to return, maximum 500
-	private String[] permittedCarrier; // A list of 2-letter IATA airline designators.
-	private String[] prohibitedCarrier; // A list of 2-letter IATA airline designators. Exclude slices that use these carriers.
-	private String preferredCabin; // Allowed values are COACH, PREMIUM_COACH, BUSINESS, and FIRST.
-	
+	private int maxConnectionDuration; // The longest connection between two
+										// legs, in minutes
+	private int solutions; // he number of solutions to return, maximum 500
+	private List<String> permittedCarrier; // A list of 2-letter IATA airline
+											// designators.
+	private List<String> prohibitedCarrier; // A list of 2-letter IATA airline
+											// designators. Exclude slices that
+											// use these carriers.
+	private String preferredCabin; // Allowed values are COACH, PREMIUM_COACH,
+									// BUSINESS, and FIRST.
+
 	private List<TripOption> tripResults;
 	private Future<List<TripOption>> future;
 
+	/**
+	 * Default constructor will set the default values for the search
+	 */
 	public Search() {
-		// TO-DO set default values
-		// default date can be set to today's date
+		adultCount = 1;
+		childCount = 0;
+		infantInLapCount = 0;
+		infantInSeatCount = 0;
+		maxPrice = "USD1000000"; // 1 million dollard by default
+		solutions = 500;
+		preferredCabin = null; // no preferrence
+		maxConnectionDuration = 72 * 60; // 3 days
+		permittedCarrier = null; // no preferrence
+		prohibitedCarrier = null; // no preferrence
+		maxStops = 10;
 	}
 
 	/**
@@ -60,7 +80,7 @@ public class Search {
 	public void startSearch() {
 		ExecutorService executor = Executors.newSingleThreadExecutor();
 		Callable<List<TripOption>> callable;
-		
+
 		callable = new Callable<List<TripOption>>() {
 			@Override
 			public List<TripOption> call() throws IOException, MalformedURLException {
@@ -71,20 +91,30 @@ public class Search {
 				}
 
 				PassengerCounts passengers = new PassengerCounts();
-				passengers.setAdultCount(1);
+				passengers.setAdultCount(adultCount);
+				passengers.setChildCount(childCount);
+				passengers.setInfantInLapCount(infantInLapCount);
+				passengers.setInfantInSeatCount(infantInSeatCount);
+				passengers.setSeniorCount(seniorCount);
 
 				List<SliceInput> slices = new ArrayList<SliceInput>();
 
 				SliceInput slice = new SliceInput();
-				slice.setOrigin(departure); 
+				slice.setOrigin(departure);
 				slice.setDestination(destination);
 				slice.setDate(departureDate);
-				
+				slice.setMaxStops(maxStops);
+				slice.setMaxConnectionDuration(maxConnectionDuration);
+				slice.setPreferredCabin(preferredCabin);
+				slice.setPermittedCarrier(permittedCarrier);
+				slice.setProhibitedCarrier(prohibitedCarrier);
+
 				slices.add(slice);
 
 				TripOptionsRequest request = new TripOptionsRequest();
-				request.setSolutions(10);
+				request.setSolutions(solutions);
 				request.setPassengers(passengers);
+				request.setMaxPrice(maxPrice);
 				request.setSlice(slices);
 
 				TripsSearchRequest parameters = new TripsSearchRequest();
@@ -97,12 +127,12 @@ public class Search {
 				return tripResults;
 			}
 		};
-		
-		 future = executor.submit(callable);
+
+		future = executor.submit(callable);
 		executor.shutdown();
 	}
-	
-	List<TripOption> getResults(){
+
+	List<TripOption> getResults() {
 		try {
 			return future.get(10, TimeUnit.SECONDS);
 		} catch (InterruptedException | ExecutionException | TimeoutException e) {
@@ -110,174 +140,115 @@ public class Search {
 		}
 		return null;
 	}
-	
-	
-	/**
-	 * @return the departure
-	 */
-	public String getDeparture() {
-		return departure;
-	}
 
 	/**
-	 * @param departure the departure to set
+	 * @param departure
+	 *            the departure to set
 	 */
 	public void setDeparture(String departure) {
 		this.departure = departure;
 	}
 
 	/**
-	 * @return the destination
-	 */
-	public String getDestination() {
-		return destination;
-	}
-
-	/**
-	 * @param destination the destination to set
+	 * @param destination
+	 *            the destination to set
 	 */
 	public void setDestination(String destination) {
 		this.destination = destination;
 	}
 
 	/**
-	 * @return the departureDate
-	 */
-	public String getDepartureDate() {
-		return departureDate;
-	}
-
-	/**
-	 * @param departureDate the departureDate to set
+	 * @param departureDate
+	 *            the departureDate to set
 	 */
 	public void setDepartureDate(String departureDate) {
 		this.departureDate = departureDate;
 	}
-	
+
 	/**
-	 * @return the maxPrice
+	 * @param maxPrice
+	 *            the maxPrice to set
 	 */
-	public int getMaxPrice() {
-		return maxPrice;
+	public void setMaxPrice(double maxPrice) {
+		this.maxPrice = "USD" + maxPrice;
 	}
 
 	/**
-	 * @param maxPrice the maxPrice to set
-	 */
-	public void setMaxPrice(int maxPrice) {
-		this.maxPrice = maxPrice;
-	}
-
-	/**
-	 * @return the adultCount
-	 */
-	public int getAdultCount() {
-		return adultCount;
-	}
-
-	/**
-	 * @param adultCount the adultCount to set
+	 * @param adultCount
+	 *            the adultCount to set
 	 */
 	public void setAdultCount(int adultCount) {
 		this.adultCount = adultCount;
 	}
 
 	/**
-	 * @return the childCount
-	 */
-	public int getChildCount() {
-		return childCount;
-	}
-
-	/**
-	 * @param childCount the childCount to set
+	 * @param childCount
+	 *            the childCount to set
 	 */
 	public void setChildCount(int childCount) {
 		this.childCount = childCount;
 	}
 
 	/**
-	 * @return the seniorCount
-	 */
-	public int getSeniorCount() {
-		return seniorCount;
-	}
-
-	/**
-	 * @param seniorCount the seniorCount to set
+	 * @param seniorCount
+	 *            the seniorCount to set
 	 */
 	public void setSeniorCount(int seniorCount) {
 		this.seniorCount = seniorCount;
 	}
 
 	/**
-	 * @return the maxStops
-	 */
-	public int getMaxStops() {
-		return maxStops;
-	}
-
-	/**
-	 * @param maxStops the maxStops to set
+	 * @param maxStops
+	 *            the maxStops to set
 	 */
 	public void setMaxStops(int maxStops) {
 		this.maxStops = maxStops;
 	}
 
 	/**
-	 * @return the maxConnectionDuration
-	 */
-	public int getMaxConnectionDuration() {
-		return maxConnectionDuration;
-	}
-
-	/**
-	 * @param maxConnectionDuration the maxConnectionDuration to set
+	 * @param maxConnectionDuration
+	 *            the maxConnectionDuration to set
 	 */
 	public void setMaxConnectionDuration(int maxConnectionDuration) {
 		this.maxConnectionDuration = maxConnectionDuration;
 	}
 
 	/**
-	 * @return the permittedCarrier
+	 * @param permittedCarrier
+	 *            the permittedCarrier to set
 	 */
-	public String[] getPermittedCarrier() {
-		return permittedCarrier;
-	}
-
-	/**
-	 * @param permittedCarrier the permittedCarrier to set
-	 */
-	public void setPermittedCarrier(String[] permittedCarrier) {
+	public void setPermittedCarrier(List<String> permittedCarrier) {
 		this.permittedCarrier = permittedCarrier;
 	}
 
 	/**
-	 * @return the prohibitedCarrier
+	 * @param prohibitedCarrier
+	 *            the prohibitedCarrier to set
 	 */
-	public String[] getProhibitedCarrier() {
-		return prohibitedCarrier;
-	}
-
-	/**
-	 * @param prohibitedCarrier the prohibitedCarrier to set
-	 */
-	public void setProhibitedCarrier(String[] prohibitedCarrier) {
+	public void setProhibitedCarrier(List<String> prohibitedCarrier) {
 		this.prohibitedCarrier = prohibitedCarrier;
 	}
 
 	/**
-	 * @return the preferredCabin
-	 */
-	public String getPreferredCabin() {
-		return preferredCabin;
-	}
-
-	/**
-	 * @param preferredCabin the preferredCabin to set
+	 * @param preferredCabin
+	 *            the preferredCabin to set
 	 */
 	public void setPreferredCabin(String preferredCabin) {
 		this.preferredCabin = preferredCabin;
+	}
+
+	/**
+	 * @param infantInLapCount
+	 */
+	public void setInfantInLapCount(int infantInLapCount) {
+		this.infantInLapCount = infantInLapCount;
+	}
+
+	/**
+	 * @param infantInSeatCount2
+	 */
+	public void setInfantInSeatCount(int infantInSeatCount) {
+		this.infantInSeatCount = infantInSeatCount;
 	}
 
 }
