6246037adf932d2ad1947b9406a2e123c7405bb6
Youcef Laidi <yocef2@gmail.com>
2017-03-31 13:13:10 -0400

    Show results in separate frame

--- src/SearchResults.java
+++ src/SearchResults.java
@@ -1,8 +1,9 @@
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.util.List;
 
-import javax.swing.JPanel;
+import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
@@ -19,28 +20,26 @@ import com.google.api.services.qpxExpress.model.TripOption;
  *
  */
 
+public class SearchResults extends JFrame {
 
-public class SearchResults extends JPanel {
-	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	static DefaultTableModel dtm = new DefaultTableModel(0, 0);
-	Search s;
 
-	public SearchResults() {
-		
-		super(new GridLayout(1, 0));
+	static DefaultTableModel dtm = new DefaultTableModel(0, 0);
+
+	public SearchResults(Search s) {
+
+		new GridLayout(1, 0);
 
 		String[] columnNames = { "Airline", "Departure", "Arrival", "Duration", "Price" };
-		
+
 		dtm.setColumnIdentifiers(columnNames);
 
 		JTable table = new JTable();
 		table.setModel(dtm);
-		
+
 		JTableHeader header = table.getTableHeader();
 		header.setFont(new Font("Dialog", Font.BOLD, 18));
 
@@ -48,22 +47,23 @@ public class SearchResults extends JPanel {
 		table.setEnabled(false);
 		table.setFont(new Font("Arial", Font.PLAIN, 20));
 		table.setRowHeight(30);
-	
+		populateResults(s);
+
 		JScrollPane scrollPane = new JScrollPane(table);
 		add(scrollPane);
 
 		setVisible(true);
-	}
-	
-	
-	public void setSearchParameters(Search s){
-		this.s = s;
+		setPreferredSize(new Dimension(850, 650));
+		setTitle("Results");
+		pack();
 	}
 
 	/**
 	 * 
 	 */
-	public void getResults(Search s) {
+	public void populateResults(Search s) {
+
+		dtm.setRowCount(0); // clear table if previous results are there
 		List<TripOption> tripResults = s.getResults();
 
 		for (int i = 0; i < tripResults.size(); i++) {
@@ -78,7 +78,7 @@ public class SearchResults extends JPanel {
 				List<SegmentInfo> segInfo = sliceInfo.get(j).getSegment();
 
 				for (int k = 0; k < segInfo.size(); k++) {
-					//String bookingCode = segInfo.get(k).getBookingCode();
+					// String bookingCode = segInfo.get(k).getBookingCode();
 					FlightInfo flightInfo = segInfo.get(k).getFlight();
 					String flightNum = flightInfo.getNumber();
 					String flightCarrier = flightInfo.getCarrier();
@@ -89,14 +89,13 @@ public class SearchResults extends JPanel {
 					List<LegInfo> leg = segInfo.get(k).getLeg();
 
 					for (int l = 0; l < leg.size(); l++) {
-
-						//String aircraft = leg.get(l).getAircraft();
+						// String aircraft = leg.get(l).getAircraft();
 						String arrivalTime = leg.get(l).getArrivalTime();
 						String departureTime = leg.get(l).getDepartureTime();
 						String dest = leg.get(l).getDestination();
-						//String destTer = leg.get(l).getDestinationTerminal();
+						// String destTer = leg.get(l).getDestinationTerminal();
 						String origin = leg.get(l).getOrigin();
-						//String originTer = leg.get(l).getOriginTerminal();
+						// String originTer = leg.get(l).getOriginTerminal();
 						int durationLeg = leg.get(l).getDuration();
 						int mil = leg.get(l).getMileage();
 
@@ -112,15 +111,13 @@ public class SearchResults extends JPanel {
 				String price = tripResults.get(i).getPricing().get(0).getSaleTotal();
 				tempFlight.setPrice(price);
 
-				addFlight(tempFlight);
+				// add flight to table
+				dtm.addRow(new Object[] { tempFlight.getAirLineName(), tempFlight.getOrigin(),
+						tempFlight.getDestination(), tempFlight.getDuration(), tempFlight.getPrice() });
+
 			}
-			
+
 		}
-	}
-	
-	public void addFlight(Flight flight){
-	    dtm.addRow(new Object[]{ flight.getAirLine(), flight.getOrigin(), 
-	    		flight.getDestination(), flight.getDuration(), flight.getPrice() });
 	}
 
 }
