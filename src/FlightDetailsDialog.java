e4bf9d452828fa0ce34a1338a2268b08a5d9fd7f
Youcef Laidi <yocef2@gmail.com>
2017-04-12 13:41:05 -0400

    Added Details dialog for the user to see individual flight details

---
+++ src/FlightDetailsDialog.java
@@ -0,0 +1,54 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
+/**
+ * @author Youcef Laidi
+ *
+ */
+public class FlightDetailsDialog extends JDialog {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private final JPanel contentPanel = new JPanel();
+
+	/**
+	 * Create the dialog.
+	 */
+	public FlightDetailsDialog() {
+		setBounds(100, 100, 450, 300);
+		setVisible(true);
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setLayout(new FlowLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton cancelButton = new JButton("Close");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						setVisible(false);
+						dispose();
+					}
+				});
+				cancelButton.setActionCommand("Close");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+
+}
--- src/SearchResults.java
+++ src/SearchResults.java
@@ -1,14 +1,24 @@
 import com.google.api.services.qpxExpress.model.*;
 
+import javax.swing.DefaultCellEditor;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.UIManager;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
-
+import javax.swing.table.TableCellRenderer;
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.List;
 
 /**
@@ -18,18 +28,23 @@ import java.util.List;
 
 public class SearchResults extends JFrame {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-	
-	private final DefaultTableModel dtm = new DefaultTableModel(0, 0);
+
+	private DefaultTableModel dtm;
 
 	public SearchResults(Search s) {
 
 		new GridLayout(1, 0);
 
-		String[] columnNames = { "Airline", "Departure", "Arrival", "Duration", "Price" };
+		String[] columnNames = { "Airline", "Departure", "Arrival", "Duration", "Price", "Details" };
+
+		dtm = new DefaultTableModel(0, 0) {
+			private static final long serialVersionUID = 1L;
+
+			public boolean isCellEditable(int row, int column) {
+				return column == 5;
+			}
+		};
 
 		dtm.setColumnIdentifiers(columnNames);
 
@@ -40,17 +55,22 @@ public class SearchResults extends JFrame {
 		header.setFont(new Font("Dialog", Font.BOLD, 18));
 
 		table.setFillsViewportHeight(true);
-		table.setEnabled(false);
 		table.setFont(new Font("Arial", Font.PLAIN, 20));
 		table.setRowHeight(30);
-		
+
+		table.getColumnModel().getColumn(5).setCellRenderer(new ButtonRenderer());
+		table.getColumnModel().getColumn(5).setCellEditor(new TableRenderer(new JCheckBox()));
+		table.setPreferredScrollableViewportSize(table.getPreferredSize());
+		table.setShowHorizontalLines(true);
+		table.setShowVerticalLines(false);
+
 		populateResults(s);
 
 		JScrollPane scrollPane = new JScrollPane(table);
 		add(scrollPane);
 
 		setVisible(true);
-		setPreferredSize(new Dimension(850, 650));
+		setPreferredSize(new Dimension(900, 650));
 		setTitle("Results");
 		pack();
 	}
@@ -111,17 +131,92 @@ public class SearchResults extends JFrame {
 
 					// add flight to table
 					dtm.addRow(new Object[] { tempFlight.getAirLineName(), tempFlight.getOrigin(),
-							tempFlight.getDestination(), tempFlight.getDuration(), tempFlight.getPrice() });
+							tempFlight.getDestination(), tempFlight.getDuration(), tempFlight.getPrice(), "Details" });
 				});
-				
-				
 
 			});
 		} else {
 			System.out.println("No results found.");
 			System.out.println("from " + s.getDeparture() + " to " + s.getDestination());
-
 		}
 	}
 
+}
+
+/**
+ * 
+ * @author Youcef
+ * 
+ * Code below was originated from user Bitmap at:
+ * http://stackoverflow.com/a/10348919/3850242
+ * 
+ * I have optimized it to suit JTable needs on 4/12/2017
+ *
+ */
+class ButtonRenderer extends JButton implements TableCellRenderer {
+
+	private static final long serialVersionUID = 1L;
+
+	public ButtonRenderer() {
+		setOpaque(true);
+	}
+
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
+			int row, int column) {
+		setForeground(Color.black);
+		setBackground(UIManager.getColor("Button.background"));
+		setText((value == null) ? "" : value.toString());
+		return this;
+	}
+}
+
+class TableRenderer extends DefaultCellEditor {
+
+	private static final long serialVersionUID = 1L;
+	private JButton button;
+	private String label;
+	private boolean clicked;
+	private int row;
+	private JTable table;
+
+	public TableRenderer(JCheckBox checkBox) {
+		super(checkBox);
+		button = new JButton();
+		button.setOpaque(true);
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				fireEditingStopped();
+			}
+		});
+	}
+
+	public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
+		this.table = table;
+		this.row = row;
+
+		button.setForeground(Color.black);
+		button.setBackground(UIManager.getColor("Button.background"));
+		label = (value == null) ? "" : value.toString();
+		button.setText(label);
+		clicked = true;
+		return button;
+	}
+
+	public Object getCellEditorValue() {
+		if (clicked) {
+			new FlightDetailsDialog();
+			//JOptionPane.showMessageDialog(button, "Column with Value: " + table.getValueAt(row, 1) + " -  Clicked!");
+		}
+		clicked = false;
+		return new String(label);
+	}
+
+	public boolean stopCellEditing() {
+		clicked = false;
+		return super.stopCellEditing();
+	}
+
+	protected void fireEditingStopped() {
+		super.fireEditingStopped();
+	}
 }
