d91e7669cf20b1c063a402b327dfb45409b11600
Youcef Laidi <yocef2@gmail.com>
2017-04-21 16:42:20 -0400

    Import only used componenets

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
-import javax.swing.*;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Font;
+
+import javax.swing.Box;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.WindowConstants;
 
 /**
  * @author Youcef Laidi
