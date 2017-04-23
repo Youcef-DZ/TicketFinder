e413bc91b6d47188b72bd1cc0df7054e21830ef7
Youcef Laidi <yocef2@gmail.com>
2017-04-23 09:49:13 -0400

    Display more details about each flight

--- src/TripDetailsDialog.java
+++ src/TripDetailsDialog.java
@@ -1,130 +1,151 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Toolkit;
 
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
 
 import java.text.SimpleDateFormat;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
-
 
 /**
  * @author Youcef Laidi
  */
 public class TripDetailsDialog extends JDialog {
 
-    /**
-     *
-     */
-    private static final long serialVersionUID = 1L;
-    SimpleDateFormat timeFormat = new SimpleDateFormat("HH::mm");
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+	SimpleDateFormat timeFormat = new SimpleDateFormat("HH::mm");
 
-    /**
-     * Create the dialog.
-     */
-    public TripDetailsDialog(Trip selectedTrip) {
-        setBounds(100, 100, 850, 400);
-        setVisible(true);
-        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+	/**
+	 * Create the dialog.
+	 */
+	public TripDetailsDialog(Trip selectedTrip) {
 
-        getContentPane().setLayout(new BorderLayout());
-        JPanel contentPanel = new JPanel();
-        contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 
-        Flight selectedFlight = selectedTrip.getFlight(0);
-        String airline = selectedFlight.getAirLineName();
-        String originAirport = selectedFlight.getOriginAirportName();
-        String originCityName = selectedFlight.getOriginCityName();
-        String departureTime = selectedFlight.getDepartureTime();
+		setBounds(100, 100, (int) (dim.getWidth() - 200), 400);
+		setVisible(true);
+		setResizable(false);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
-        String destinationAirport = selectedFlight.getDestinationAirportName();
-        String destinationCityName = selectedFlight.getDestinationCityName();
-        String arrivalTime = selectedFlight.getArrivalTime();
+		getContentPane().setLayout(new BorderLayout());
+		Flight selectedFlight = selectedTrip.getFlight(0);
 
-        //int duration = selectedFlight.getDuration();
-        //String price = selectedFlight.getPrice();
+		JPanel contentPanel = singleFlightPanel(selectedFlight);
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
 
-        getContentPane().add(contentPanel, BorderLayout.CENTER);
-        JLabel lblairline;
-        {
-            lblairline = new JLabel(airline);
-        }
+		JPanel buttonPane = new JPanel();
+		buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		getContentPane().add(buttonPane, BorderLayout.SOUTH);
 
-        JLabel lblDepartdate = new JLabel(selectedFlight.getDepartureDate());
-        JLabel lblDeparturecity = new JLabel(originCityName + " (" + originAirport + ")");        
-        JLabel lblDeparturetime = new JLabel(departureTime);        
-        JLabel lblArrivaltime = new JLabel(arrivalTime);        
-        JLabel lblArrivalcity = new JLabel(destinationCityName+ " (" + destinationAirport + ")");        
-        JLabel lblDuration = new JLabel(selectedFlight.getFormatedDuration());      
-        JLabel lblMileage = new JLabel(selectedFlight.getMileage() + " Miles");        
-        JLabel lblAircraft = new JLabel("aircraft");
-        
-        GroupLayout gl_contentPanel = new GroupLayout(contentPanel);
-        gl_contentPanel.setHorizontalGroup(
-        	gl_contentPanel.createParallelGroup(Alignment.LEADING)
-        		.addGroup(gl_contentPanel.createSequentialGroup()
-        			.addContainerGap()
-        			.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
-        				.addComponent(lblairline)
-        				.addComponent(lblDuration))
-        			.addPreferredGap(ComponentPlacement.RELATED)
-        			.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
-        				.addComponent(lblDepartdate)
-        				.addComponent(lblMileage))
-        			.addGap(18)
-        			.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
-        				.addGroup(gl_contentPanel.createSequentialGroup()
-        					.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
-        						.addComponent(lblDeparturetime)
-        						.addComponent(lblArrivaltime))
-        					.addPreferredGap(ComponentPlacement.RELATED, 242, Short.MAX_VALUE)
-        					.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
-        						.addComponent(lblArrivalcity)
-        						.addComponent(lblDeparturecity))
-        					.addGap(102))
-        				.addGroup(gl_contentPanel.createSequentialGroup()
-        					.addComponent(lblAircraft)
-        					.addContainerGap(541, Short.MAX_VALUE))))
-        );
-        gl_contentPanel.setVerticalGroup(
-        	gl_contentPanel.createParallelGroup(Alignment.LEADING)
-        		.addGroup(gl_contentPanel.createSequentialGroup()
-        			.addContainerGap()
-        			.addGroup(gl_contentPanel.createParallelGroup(Alignment.BASELINE)
-        				.addComponent(lblairline)
-        				.addComponent(lblDepartdate)
-        				.addComponent(lblDeparturecity)
-        				.addComponent(lblDeparturetime))
-        			.addGap(31)
-        			.addGroup(gl_contentPanel.createParallelGroup(Alignment.BASELINE)
-        				.addComponent(lblArrivaltime)
-        				.addComponent(lblArrivalcity))
-        			.addGap(71)
-        			.addGroup(gl_contentPanel.createParallelGroup(Alignment.BASELINE)
-        				.addComponent(lblDuration)
-        				.addComponent(lblMileage)
-        				.addComponent(lblAircraft))
-        			.addContainerGap(117, Short.MAX_VALUE))
-        );
-        contentPanel.setLayout(gl_contentPanel);
-        {
-            JPanel buttonPane = new JPanel();
-            buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-            getContentPane().add(buttonPane, BorderLayout.SOUTH);
-            {
-                JButton cancelButton = new JButton("Close");
-                cancelButton.addActionListener(e -> {
-                    setVisible(false);
-                    dispose();
-                });
-                cancelButton.setActionCommand("Close");
-                buttonPane.add(cancelButton);
-            }
-        }
-    }
+		JButton cancelButton = new JButton("Close");
+		cancelButton.addActionListener(e -> {
+			setVisible(false);
+			dispose();
+		});
+		
+		buttonPane.add(cancelButton);
+	}
+
+	/**
+	 * @param selectedFlight
+	 */
+	private JPanel singleFlightPanel(Flight selectedFlight) {
+		JPanel contentPanel = new JPanel();
+		contentPanel.setBackground(Color.WHITE);
+		setBorder(contentPanel, "Flight");
+
+		String originAirport = selectedFlight.getOriginAirportName();
+		String originCityName = selectedFlight.getOriginCityName();
+		String originTerminal = selectedFlight.getOriginTerminal();
+		String destinationAirport = selectedFlight.getDestinationAirportName();
+		String destinationCityName = selectedFlight.getDestinationCityName();
+		String arrivalTerminal = selectedFlight.getDestTerminal();
+
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+
+		JLabel lblairline = new JLabel(selectedFlight.getAirLineName());
+		JLabel lblDepartdate = new JLabel(selectedFlight.getDepartureDate());
+		JLabel lblDeparturecity = new JLabel(originCityName + " (" + originAirport + ")");
+		JLabel lblDeparturetime = new JLabel(selectedFlight.getDepartureTime() + "  Terminal: " + originTerminal);
+		JLabel lblArrivaltime = new JLabel(selectedFlight.getArrivalTime() + "  Terminal: " + arrivalTerminal);
+		JLabel lblArrivalcity = new JLabel(destinationCityName + " (" + destinationAirport + ")");
+		JLabel lblDuration = new JLabel("Duration: " + selectedFlight.getFormatedDuration());
+		JLabel lblMileage = new JLabel(selectedFlight.getMileage() + " Miles");
+		JLabel lblAircraft = new JLabel(selectedFlight.getAircraft());
+		JLabel lblFlight = new JLabel("Flight# " + selectedFlight.getFlightNumber());
+
+		GroupLayout gl_contentPanel = new GroupLayout(contentPanel);
+		gl_contentPanel.setHorizontalGroup(
+			gl_contentPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPanel.createSequentialGroup()
+					.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPanel.createSequentialGroup()
+							.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblairline)
+								.addComponent(lblFlight)
+								.addGroup(gl_contentPanel.createSequentialGroup()
+									.addComponent(lblDuration)
+									.addGap(24)
+									.addComponent(lblDepartdate)))
+							.addGap(49)
+							.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblDeparturetime)
+								.addComponent(lblArrivaltime))
+							.addGap(56)
+							.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblArrivalcity)
+								.addComponent(lblDeparturecity)))
+						.addComponent(lblMileage)
+						.addComponent(lblAircraft))
+					.addContainerGap(887, Short.MAX_VALUE))
+		);
+		gl_contentPanel.setVerticalGroup(
+			gl_contentPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPanel.createSequentialGroup()
+					.addGroup(gl_contentPanel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblairline)
+						.addComponent(lblDepartdate)
+						.addComponent(lblDeparturetime)
+						.addComponent(lblDeparturecity))
+					.addGap(21)
+					.addGroup(gl_contentPanel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblArrivaltime)
+						.addComponent(lblFlight)
+						.addComponent(lblArrivalcity))
+					.addGap(12)
+					.addComponent(lblDuration)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(lblMileage)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(lblAircraft)
+					.addContainerGap(114, Short.MAX_VALUE))
+		);
+		contentPanel.setLayout(gl_contentPanel);
+		return contentPanel;
+	}
+
+	/**
+	*
+	*/
+	private void setBorder(JPanel pnl, String title) {
+		Border border = new TitledBorder(UIManager.getBorder("TitledBorder.border"), "\t" + title + "\t",
+				TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0));
+		Border margin = new EmptyBorder(10, 10, 10, 10);
+		pnl.setBorder(new CompoundBorder(border, margin));
+	}
 }
